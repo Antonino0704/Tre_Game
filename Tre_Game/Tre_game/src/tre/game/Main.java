219e8e2fbf2cbd40787c57662debc17170e8d3e8
Antoino0704 <76400595+Antoino0704@users.noreply.github.com>
2021-05-16 00:47:28 +0200

    Update Main.java

--- Tre_Game/Tre_game/src/tre/game/Main.java
+++ Tre_Game/Tre_game/src/tre/game/Main.java
@@ -2,12 +2,13 @@ package tre.game;
 
 public class Main {
 	private static Random random;
+	private static Gui gui;
 	
 	public static void main(String[] args) {
 		random = new Random();
 		random.randomNum();
 		
-		Gui gui = new Gui();
+		gui = new Gui();
 		gui.actionButton1(random);
 		gui.actionButton2(random);
 		gui.actionButton3(random);
